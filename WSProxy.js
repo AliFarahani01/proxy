ef3625966a58a1c324274f3c103f4f537dac8ae3
Lllll <alifarahani31301@gmail.com>
2025-11-26 22:21:22 +0330

    Update WSProxy.js

--- WSProxy.js
+++ WSProxy.js
@@ -11,6 +11,8 @@ export class WSProxy {
     const [client, server] = Object.values(new WebSocketPair());
 
     this.sockets.add(client);
+client.addEventListener("message", evt => { if (server.readyState === 1) server.send(evt.data); });
+server.addEventListener("message", evt => { if (client.readyState === 1) client.send(evt.data); });
 
     client.addEventListener("message", evt => { try { server.send(evt.data); } catch {} });
     server.addEventListener("message", evt => { try { client.send(evt.data); } catch {} });
