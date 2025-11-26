9fbb9563b508b62453d6acfe456b7c6a7256b425
Lllll <alifarahani31301@gmail.com>
2025-11-26 22:19:18 +0330

    Update WSProxy.js

--- WSProxy.js
+++ WSProxy.js
@@ -5,22 +5,15 @@ export class WSProxy {
   }
 
   async fetch(req) {
-    // WebSocket upgrade
     if (req.headers.get("Upgrade") !== "websocket") {
       return new Response("WebSocket required", { status: 400 });
     }
     const [client, server] = Object.values(new WebSocketPair());
-    const url = new URL(req.url);
-    const targetUrl = decodeURIComponent(url.searchParams.get("url"));
-    
+
     this.sockets.add(client);
 
-    client.addEventListener("message", evt => {
-      try { server.send(evt.data); } catch {}
-    });
-    server.addEventListener("message", evt => {
-      try { client.send(evt.data); } catch {}
-    });
+    client.addEventListener("message", evt => { try { server.send(evt.data); } catch {} });
+    server.addEventListener("message", evt => { try { client.send(evt.data); } catch {} });
 
     const cleanup = () => this.sockets.delete(client);
     client.addEventListener("close", cleanup);
