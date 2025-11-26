9d4348d20e7ac43917a2543f1605211b95e9a191
Lllll <alifarahani31301@gmail.com>
2025-11-26 22:04:12 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,40 +1,6 @@
 import { WSProxy } from './wsproxy.js';
 
 
-export class WSProxy {
-  constructor(state, env) {
-    this.state = state;
-    this.sockets = new Set();
-  }
-
-  async fetch(req) {
-    // WebSocket upgrade
-    if (req.headers.get("Upgrade") !== "websocket") {
-      return new Response("WebSocket required", { status: 400 });
-    }
-    const [client, server] = Object.values(new WebSocketPair());
-    const url = new URL(req.url);
-    const targetUrl = decodeURIComponent(url.searchParams.get("url"));
-    
-    this.sockets.add(client);
-
-    client.addEventListener("message", evt => {
-      try { server.send(evt.data); } catch {}
-    });
-    server.addEventListener("message", evt => {
-      try { client.send(evt.data); } catch {}
-    });
-
-    const cleanup = () => this.sockets.delete(client);
-    client.addEventListener("close", cleanup);
-    server.addEventListener("close", cleanup);
-
-    return new Response(null, { status: 101, webSocket: client });
-  }
-}
-
-
-
 const NODES = [
   "https://1.1.1.1/",
   "https://8.8.8.8/",
